95f848dec82e551cd3dd6371ee924a45933c45bf
Mustafa Motiwala <mustafa@motiwala.ca>
2023-05-06 11:28:42 -0400

    implemented generate_tex

--- generate_tex.py
+++ generate_tex.py
@@ -1,7 +1,105 @@
 import sys
+from more_itertools import with_iter
 
-def main():
-    pass
+TOK_BEGIN = ';@begin '
+TOK_END = ';@end'
+TOK_COMMENT = ';;;'
+
+class Container:
+    def __init__(self):
+        self.objects = []
+
+    def append(self, obj):
+        self.objects.append(obj)
+
+    def evaluate(self) -> str:
+        raise NotImplementedError
+    
+class Wrapper(Container):
+    def __init__(self, arg: str):
+        self.arg = arg
+        super().__init__()
+    
+    def wrap(self, s) -> str:
+        raise NotImplementedError
+
+    def evaluate(self) -> str:
+        return self.wrap(''.join(obj.evaluate() for obj in self.objects))
+    
+    def __repr__(self):
+        return f'{self.__class__.__name__}("{self.arg}")'
+    
+    def __str__(self):
+        return repr(self)
+
+class Fragment(Wrapper):
+    def wrap(self, s):
+        return s
+    
+class Section(Wrapper):
+    def wrap(self, s):
+        return f'\\subsection{{{self.arg.strip()}}}\n{s}'
+    
+class CodeSection(Wrapper):
+    def wrap(self, s):
+        return (
+            f'\\subsubsection{{{self.arg.strip()}}}\n'
+            f'\\begin{{minted}}{{scheme}}\n'
+            f'{s}'
+            f'\\end{{minted}}\n'
+        )
+
+class Line(Container):
+    def __init__(self, content):
+        self.content = content
+        super().__init__()
+    
+    def evaluate(self) -> str:
+        return self.content
+    
+
+def get_wrapper(name):
+    return {
+        'section': Section,
+        'code': CodeSection 
+    }.get(name, Wrapper)
+    
+def parse(lines):
+    current = Fragment('')
+    stack = [current]
+    
+    started = False
+    for i, line in enumerate(lines, start=1):
+        # print(f'{i}: {stack}')
+        if line.strip() == '#lang pie':
+            started = True
+
+        elif not started:
+            continue
+
+        elif line.startswith(TOK_COMMENT):
+            continue
+
+        elif line.startswith(TOK_BEGIN):
+            _, name, *arg = line.split(' ')
+            arg = ' '.join(arg)
+            wrapper = get_wrapper(name)(arg)
+            current.append(wrapper)
+            current = wrapper
+            stack.append(current)
+
+        elif line.startswith(TOK_END):
+            stack.pop()
+            current = stack[-1]
+
+        else:
+            current.append(Line(line))
+    
+    return current.evaluate()
+
+def main(fn):
+    lines = with_iter(open(fn))
+    print(parse(lines))
 
 if __name__ == "__main__":
     raise SystemExit(main(*sys.argv[1:]))
